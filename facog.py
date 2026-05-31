5c521f06aea50bd9fcab4a267c30e65253e32b79
Who-again <ardeeznutz@gmail.com>
2026-05-31 12:39:13 +0800

    More purchaseable items!!

--- facog.py
+++ facog.py
@@ -1,31 +1,37 @@
-#Wsp, so this is my first ever python game project, the idea came after i asked gemini for a mini beginner project, while most of the code here are
-#Not AI assisted, i allowed Gemini to give me hints for codes i should use and actively encourage me to read python documentation.
-#I will also be implementing the things i have learnt from Harvard's CS50p course
+# Wsp, so this is my first ever python game project, the idea came after i asked gemini for a mini beginner project, while most of the code here are
+# Not AI assisted, i allowed Gemini to give me hints for codes i should use and actively encourage me to read python documentation.
+# I will also be implementing the things i have learnt from Harvard's CS50p course
 
-#Pls enjoy.         //CodeRPG//
+# Pls enjoy.         //CodeRPG//
+
+weaponList = ["Sword"]
+consumableList = ["Health potion", "Bread", "Apple", "Pudding", "Bandage"]
 
 
-
-def main(): #Main game
-    game_running = True     #Game can be switched to false which will stop the program if the user types in "Quit"
+def main():  # Main game
+    game_running = True  # Game can be switched to false which will stop the program if the user types in "Quit"
     inventory = ["Health potion", "Bread"]
     health = int(100)
     gold = int(1000)
 
     print("Welcome to the market game thingy mejiggy i have no fucking clue")
-    print("Choose what you want to use Numbers only, starts from 0, type in Shop to enter Shop")
+    print(
+        "Choose what you want to use Numbers only, starts from 0, type in Shop to enter Shop"
+    )
 
-    while game_running:     #While loop, so the player can choose when to stop and also makes it that the game doesnt auto close after an input
+    while game_running:  # While loop, so the player can choose when to stop and also makes it that the game doesnt auto close after an input
         print("Inventory:   ", inventory)
         print("Gold:    ", gold)
         print("Health:  ", health)
 
-        player_input = input()      #This is where the player can choose to use the items in the inventory or go to the shop or etc
-        
+        player_input = input()  # This is where the player can choose to use the items in the inventory or go to the shop or etc
+
         if player_input == "Shop":
             inventory, gold = shop_mechanic(inventory, gold)
-        elif player_input .isdigit():
-            intplayer_input = int(player_input)     #made a new player input varaible specifically to convert the given strings to integers (e.g, "123" -> 123)
+        elif player_input.isdigit():
+            intplayer_input = int(
+                player_input
+            )  # made a new player input varaible specifically to convert the given strings to integers (e.g, "123" -> 123)
             if intplayer_input < 0 or intplayer_input >= len(inventory):
                 print("Out of range lil bro")
             else:
@@ -35,29 +41,42 @@ def main(): #Main game
             print("Quitting")
             game_running = False
 
-def use_item(player_input, inventory, current_health):  #Once called, this function will take the player data and use the item according to the player input
+
+def use_item(
+    player_input, inventory, current_health
+):  # Once called, this function will take the player data and use the item according to the player input
     chosen_item = inventory[player_input]
 
     if chosen_item == "Health potion":
         heal = 25
         new_health = current_health + heal
-        print("Healed by:   ",heal)
+        print("Healed by:   ", heal)
     elif chosen_item == "Bread":
         print("Why does it taste like dog shit wrapped in cat shit..")
         damage = 5
         new_health = current_health - damage
         print("Damaged by   :", damage)
+    elif chosen_item == "Sword":
+        print(
+            "It's fragile.."
+        )  # This is just a placeholder code, Sword scripts coming soon
+
     else:
         print("Out of range")
         new_health = current_health
-    inventory.pop(player_input)     #Once the function is completed it will delete the chosen item, preventing it from being used again
-    return inventory, new_health    #Returns the updated player data to the caller
+    inventory.pop(
+        player_input
+    )  # Once the function is completed it will delete the chosen item, preventing it from being used again
+    return inventory, new_health  # Returns the updated player data to the caller
 
-def shop_mechanic(inventory, gold):     # Shop mechanic implemented, just add more codes later
+
+def shop_mechanic(
+    inventory, gold
+):  # Shop mechanic implemented, just add more codes later
 
     shop_inventory = ["Sword", "Apple", "Pudding", "Bandage"]
-    print("Hello traveler, choose your item:    ",shop_inventory)
-    
+    print("Hello traveler, choose your item:    ", shop_inventory)
+
     shop_input = int(input())
     chosen_item = shop_inventory[shop_input]
 
@@ -67,13 +86,59 @@ def shop_mechanic(inventory, gold):     # Shop mechanic implemented, just add mo
     BandageCost = 10
 
     if chosen_item == "Sword":
-        print(f"Are you sure you wnat to buy {chosen_item} for {SwordCost}?")
+        print(f"Are you sure you want to buy {chosen_item} for {SwordCost}? Y/N")
         sureinput = str(input())
-        if sureinput == "Y":
+
+        if sureinput == "Y" or sureinput == "y":
             print(f"{chosen_item} bought")
             inventory.append(chosen_item)
             newgold = gold - SwordCost
-            return inventory, newgold
+
+        elif sureinput == "N" or sureinput == "n":
+            print("Purchase canceled")
+            newgold = gold
+            inventory = inventory
+    elif chosen_item == "Apple":
+        print(f"Are you sure you want to buy {chosen_item} for {AppleCost}? Y/N")
+        sureinput = str(input())
+
+        if sureinput == "Y" or sureinput == "y":
+            print(f"{chosen_item} bought")
+            inventory.append(chosen_item)
+            newgold = gold - AppleCost
+
+        elif sureinput == "N" or sureinput == "n":
+            print("Purchase canceled")
+            newgold = gold
+            inventory = inventory
+    elif chosen_item == "Pudding":
+        print(f"Are you sure you want to buy {chosen_item} for {PuddingCost}? Y/N")
+        sureinput = str(input())
+
+        if sureinput == "Y" or sureinput == "y":
+            print(f"{chosen_item} bought")
+            inventory.append(chosen_item)
+            newgold = gold - PuddingCost
+
+        elif sureinput == "N" or sureinput == "n":
+            print("Purchase canceled")
+            newgold = gold
+            inventory = inventory
+    elif chosen_item == "Bandage":
+        print(f"Are you sure you want to buy {chosen_item} for {BandageCost}? Y/N")
+        sureinput = str(input())
+
+        if sureinput == "Y" or sureinput == "y":
+            print(f"{chosen_item} bought")
+            inventory.append(chosen_item)
+            newgold = gold - BandageCost
+
+        elif sureinput == "N" or sureinput == "n":
+            print("Purchase canceled")
+            newgold = gold
+            inventory = inventory
+
+    return inventory, newgold
+
 
 main()
-    
